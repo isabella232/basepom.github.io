ca64eb4b0480299791873598e894b5467db4173e
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2022-02-22 09:57:06 -0800

    Site checkin for project inline-maven-plugin

---
+++ inline-maven-plugin/release-1.0/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.basepom.inline.mojo","l":"ArtifactIdentifier"},{"p":"org.basepom.inline.mojo","l":"HelpMojo"},{"p":"org.basepom.inline.mojo","l":"InlineDependency"},{"p":"org.basepom.inline.mojo","l":"InlineMojo"},{"p":"org.basepom.inline.mojo","l":"ScopeLimitingFilter"}];updateSearchResults();
